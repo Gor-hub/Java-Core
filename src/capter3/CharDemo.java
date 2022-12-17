5cbfd42f3d77269771e48a11d5c038e0c46ff537
Gor-hub <60032946+Gor-hub@users.noreply.github.com>
2022-12-17 14:55:29 +0100

    Initial commit

---
+++ src/capter3/CharDemo.java
@@ -0,0 +1,12 @@
+package capter3;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char chl, ch2;
+        chl = 88; // ??? ??????? ?
+        ch2 = 'Y';
+        System.out.print("chl ? ch2: ");
+        System.out.println(chl + " " + ch2);
+    }
+}
+
