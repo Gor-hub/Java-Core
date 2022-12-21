b94c8d1ce831fb2dd09665aa81705a885a6f0d47
Gor-hub <60032946+Gor-hub@users.noreply.github.com>
2022-12-21 17:24:02 +0100

    change condition for "for"

--- src/homework/homework1/IfForExample.java
+++ src/homework/homework1/IfForExample.java
@@ -4,12 +4,18 @@ public class IfForExample {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
+        int min;
+        int max;
         if (a > b) {
             System.out.println("Variable a is greater than variable b");
+            min = b;
+            max = a;
         } else {
             System.out.println("Variable b is greater than variable b ");
+            min = a;
+            max = b;
         }
-        for (int i = a; i <= b; i++) {
+        for (int i = min; i <= max; i++) {
             System.out.print(i + ", ");
         }
 
