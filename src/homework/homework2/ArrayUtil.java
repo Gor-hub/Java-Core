70ee619e8a827021972a544b62436514f152f24e
Gor-hub <60032946+Gor-hub@users.noreply.github.com>
2022-12-24 21:52:06 +0100

    add 2 classes ArrayUtil and FigurePainter2

---
+++ src/homework/homework2/ArrayUtil.java
@@ -0,0 +1,22 @@
+package homework.homework2;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+        int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+        int max = array[0];
+        int min = array[0];
+        for (int i = 0; i <= array.length - 1; i++) {
+            System.out.print(array[i] + " ");
+        }
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (max <= array[i])
+                max = array[i];
+
+            if (min >= array[i])
+                min = array[i];
+        }
+        System.out.println();
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
