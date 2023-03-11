4a889e69c3e5fba715a0fd8d93e7291777d44795
Gor Akopyan <60032946+Gor-hub@users.noreply.github.com>
2023-03-11 18:42:02 +0100

    Add package collections

--- src/homework/Collections/CollectionExample2.java
+++ src/homework/Collections/CollectionExample2.java
@@ -2,7 +2,7 @@ package homework.Collections;
 
 import java.util.*;
 
-public class CollectionExample2  {
+public class CollectionExample2 {
     public static void main(String[] args) {
         System.out.println("Please input names");
         Set<String> set = new TreeSet<>();
