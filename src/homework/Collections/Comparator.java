a759223e190fafa05304d07f88cad8d348e4b2fe
Gor Akopyan <60032946+Gor-hub@users.noreply.github.com>
2023-03-11 18:38:51 +0100

    Add package collections

--- src/homework/Collections/Comparator.java
+++
@@ -1,8 +0,0 @@
-package homework.Collections;
-
-public class Comparator implements java.util.Comparator {
-    @Override
-    public int compare(Object o1, Object o2) {
-        return 0;
-    }
-}
